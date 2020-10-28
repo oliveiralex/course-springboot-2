9da9d43fbc07d9ae27435f0628eb1bcb4231579c
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2020-10-28 14:34:06 -0300

    Service layer, component registration

---
+++ src/main/java/com/iftm/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.iftm.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iftm.course.entities.User;
+import com.iftm.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
