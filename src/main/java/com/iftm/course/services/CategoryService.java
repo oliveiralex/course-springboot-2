4afcb15237dc6ac8984128f54f15b424b7603508
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2020-11-09 11:37:57 -0300

    Challenge: category CRUD

--- src/main/java/com/iftm/course/services/CategoryService.java
+++ src/main/java/com/iftm/course/services/CategoryService.java
@@ -3,11 +3,17 @@ package com.iftm.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.iftm.course.entities.Category;
 import com.iftm.course.repositories.CategoryRepository;
+import com.iftm.course.services.exceptions.DatabaseException;
+import com.iftm.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,6 +27,36 @@ public class CategoryService {
 	
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {
+			Category entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 }
