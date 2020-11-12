d1b6c25b98a1646f28a6263ced1101360ef1fdab
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2020-11-12 13:21:13 -0300

    Challenge: CategoryDTO, ProductDTO

--- src/main/java/com/iftm/course/services/ProductService.java
+++ src/main/java/com/iftm/course/services/ProductService.java
@@ -2,12 +2,15 @@ package com.iftm.course.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.iftm.course.dto.ProductDTO;
 import com.iftm.course.entities.Product;
 import com.iftm.course.repositories.ProductRepository;
+import com.iftm.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -15,12 +18,14 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 
-	public List<Product> findAll() {
-		return repository.findAll();
+	public List<ProductDTO> findAll() {
+		List<Product> list =  repository.findAll();
+		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
 	}
 	
-	public Product findById(Long id) {
+	public ProductDTO findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		return new ProductDTO(entity);
 	}
 }
