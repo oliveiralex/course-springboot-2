a2c008ee682719a373990dd88bbcc72fc6ee62af
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2020-12-18 11:17:52 -0300

    Custom user update validation: unique email

--- src/main/java/com/iftm/course/dto/UserDTO.java
+++ src/main/java/com/iftm/course/dto/UserDTO.java
@@ -2,14 +2,30 @@ package com.iftm.course.dto;
 
 import java.io.Serializable;
 
-import com.iftm.course.entities.User;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.Length;
+
+import com.iftm.course.entities.User;
+import com.iftm.course.services.validation.UserUpdateValid;
+
+@UserUpdateValid
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotEmpty(message = "Can't be empty")
+	@Length(min = 5, max = 80, message = "Length must be between 5 and 80")
 	private String name;
+	
+	@NotEmpty(message = "Can't be empty")
+	@Email(message = "Invalid email")
 	private String email;
+	
+	@NotEmpty(message = "Can't be empty")
+	@Length(min = 8, max = 20, message = "Length must be between 8 and 20")
 	private String phone;
 	
 	public UserDTO() {
---
+++ src/main/java/com/iftm/course/services/validation/UserUpdateValidator.java
@@ -0,0 +1,55 @@
+package com.iftm.course.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.iftm.course.dto.UserDTO;
+import com.iftm.course.entities.User;
+import com.iftm.course.repositories.UserRepository;
+import com.iftm.course.resources.exceptions.FieldMessage;
+
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void initialize(UserUpdateValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserDTO dto, ConstraintValidatorContext context) {
+		
+		// Declaração pra pegar os parâmetros da requisição
+		@SuppressWarnings("unchecked")
+		Map<String, String> map =
+			(Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Long uriId = Long.parseLong(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		User user = userRepository.findByEmail(dto.getEmail());
+		if (user != null && !user.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "email already taken"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		
+		return list.isEmpty();
+	}
+}
